ee9445670023a59dd4eff0b8bd8e3e58c5f98825
duochen <duochen@yahoo.com>
2019-04-15 10:47:10 -0500

    check in code

---
+++ MQ5_Sensor_Digital/MQ5_Sensor_Digital.ino
@@ -0,0 +1,12 @@
+int sensor=7;
+int gas_value;
+
+void setup() {
+  pinMode(sensor,INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  gas_value=digitalRead(sensor);
+  Serial.println(gas_value);
+}

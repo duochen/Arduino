493ec0b40d8cd1ecfbfb3da4985dde3839f67ed2
Duo Chen <duochen@yahoo.com>
2019-06-11 14:45:23 -0500

    check in code

--- Vibration_Sensor_Logic_State/Vibration_Sensor_Logic_State.ino
+++ Vibration_Sensor_Logic_State/Vibration_Sensor_Logic_State.ino
@@ -2,6 +2,7 @@ int vibr_pin = 3;
 int LED_Pin = 13;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(vibr_pin, INPUT);
   pinMode(LED_Pin, OUTPUT);
 }
@@ -10,6 +11,8 @@ void loop() {
   int val;
   val = digitalRead(vibr_pin);
   if (val == 1) {
+    Serial.print("Val: ");
+    Serial.println(val);
     digitalWrite(LED_Pin, HIGH);
     delay(1000);
     digitalWrite(LED_Pin, LOW);
